4152a2f6bd152722c5498430374d156086806058
Noble Kalish <NobleKalish>
2021-03-02 12:56:48 -0500

    set up

--- Bomberman/group17/group17character.py
+++ Bomberman/group17/group17character.py
@@ -3,12 +3,41 @@ import sys
 
 sys.path.insert(0, '../bomberman')
 # Import necessary stuff
-from entity import CharacterEntity
+from Bomberman.bomberman.entity import CharacterEntity
 from colorama import Fore, Back
 
 
 class Group17Character(CharacterEntity):
+    # Class constructor.
+    #
+    # PARAM [string] name:      the name of this player
+    # PARAM [int]    max_depth: the maximum search depth
+    def __init__(self, name, avatar, x, y, variant):
+        super().__init__(name, avatar, x, y)
+        # Max search depth
+        self.variant = variant
+        self.variant_solutions = {
+            1: self.variant1(),
+            2: self.variant2(),
+            3: self.variant3(),
+            4: self.variant4(),
+            5: self.variant5()
+        }
 
     def do(self, wrld):
-        # Your code here
+        self.variant_solutions.get(self.variant)
+
+    def variant1(self):
+        pass
+
+    def variant2(self):
+        pass
+
+    def variant3(self):
+        pass
+
+    def variant4(self):
+        pass
+
+    def variant5(self):
         pass
--- Bomberman/group17/scenario1/variant1.py
+++ Bomberman/group17/scenario1/variant1.py
@@ -4,16 +4,16 @@ sys.path.insert(0, '../../bomberman')
 sys.path.insert(1, '..')
 
 # Import necessary stuff
-from game import Game
+from Bomberman.bomberman.game import Game
 
 # TODO This is your code!
 sys.path.insert(1, '../group17')
 
 # Uncomment this if you want the empty test character
-#from testcharacter import TestCharacter
+from Bomberman.group17.group17character import Group17Character
 
 # Uncomment this if you want the interactive character
-from interactivecharacter import InteractiveCharacter
+# from interactivecharacter import InteractiveCharacter
 
 # Create the game
 g = Game.fromfile('map.txt')
@@ -21,16 +21,17 @@ g = Game.fromfile('map.txt')
 # TODO Add your character
 
 # Uncomment this if you want the test character
-# g.add_character(TestCharacter("me", # name
-#                               "C",  # avatar
-#                               0, 0  # position
-# ))
+g.add_character(Group17Character("me", # name
+                              "C",  # avatar
+                              0, 0,  # position
+                                 1 # variant solution
+))
 
 # Uncomment this if you want the interactive character
-g.add_character(InteractiveCharacter("me", # name
-                                     "C",  # avatar
-                                     0, 0  # position
-))
+# g.add_character(InteractiveCharacter("me", # name
+#                                      "C",  # avatar
+#                                      0, 0  # position
+# ))
 
 # Run!
 
