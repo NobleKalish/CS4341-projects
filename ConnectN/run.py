a0d0406b3e5e385b5b32f794d7e484875cca503b
Noble Kalish <NobleKalish>
2021-03-02 12:16:26 -0500

    push

--- ConnectN/run.py
+++ ConnectN/run.py
@@ -7,23 +7,24 @@ import alpha_beta_agent as aba
 # random.seed(1)
 
 games = []
-games.append(game.Game(7, 6, 4, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
-games.append(game.Game(7, 6, 5, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
-games.append(game.Game(10, 8, 4, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
-games.append(game.Game(10, 8, 5, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
-for i in range(96):
-    width = random.randint(7, 10)
-    height = random.randint(6, 8)
-    token = random.randint(4, 5)
-    randomPlayer = random.randint(1, 2)
-    if randomPlayer == 1:
-        games.append(game.Game(width, height, token, agent.RandomAgent("random"), aba.AlphaBetaAgent("alphabeta")))
-    else:
-        games.append(game.Game(width, height, token, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
+games.append(game.Game(7, 6, 4, agent.InteractiveAgent("random"), aba.AlphaBetaAgent("alphabeta")))
+# games.append(game.Game(7, 6, 5, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
+# games.append(game.Game(10, 8, 4, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
+# games.append(game.Game(10, 8, 5, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
+# for i in range(96):
+#     width = random.randint(7, 10)
+#     height = random.randint(6, 8)
+#     token = random.randint(4, 5)
+#     randomPlayer = random.randint(1, 2)
+#     if randomPlayer == 1:
+#         games.append(game.Game(width, height, token, agent.RandomAgent("random"), aba.AlphaBetaAgent("alphabeta")))
+#     else:
+#         games.append(game.Game(width, height, token, aba.AlphaBetaAgent("alphabeta"), agent.RandomAgent("random")))
 # Execute the game
 wins = 0
 ties = 0
 game_num = 0
+max_times = []
 for game in games:
     game_num += 1
     print("We are playing game " + str(game_num))
@@ -33,10 +34,15 @@ for game in games:
     else:
         player = 2
     outcome = game.go()
+    if player == 1:
+        max_times.append(game.players[0].max_time)
+    else:
+        max_times.append(game.players[1].max_time)
     if outcome == player:
         wins += 1
     elif outcome == 0:
         ties += 1
 print("We won " + str(wins) + " out of " + str(game_num) + " games")
 print("We tied " + str(ties) + " times")
+print("AI had a max time of: " + str(max(max_times)))
 
